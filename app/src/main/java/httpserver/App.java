4a9fa3a17cbae150abe0eb752caccb8b7da7e4d0
antoniomikley <antonio.mikley@gmail.com>
2023-08-18 12:34:39 +0200

    got it all working. with threading even. hopefully. not tested

--- app/src/main/java/httpserver/App.java
+++ app/src/main/java/httpserver/App.java
@@ -1,10 +1,29 @@
 package httpserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class App {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        BlockingQueue<Socket> requestQueue = new LinkedBlockingQueue<Socket>();
         for (int i = 0; i < 4; i++) {
-            Thread worker = new Thread(new RequestHandler());
+            Thread worker = new Thread(new RequestHandler(requestQueue));
             worker.start();
+        }
+        try {
+            ServerSocket requestListener = new ServerSocket(8080);
+            while (true) {
+                requestQueue.put(requestListener.accept());
+            }
+        } catch (IOException err) {
+            System.err.println("Dont know why this happens.");
+            err.printStackTrace();
+        } catch (InterruptedException err) {
+            err.printStackTrace();
         }
     }
 }
--- app/src/main/java/httpserver/RequestHandler.java
+++ app/src/main/java/httpserver/RequestHandler.java
@@ -3,47 +3,31 @@ package httpserver;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
 
 public class RequestHandler implements Runnable {
+    BlockingQueue<Socket> requestQueue;
+
+    public RequestHandler(BlockingQueue<Socket> requestQueue) {
+        this.requestQueue = requestQueue;
+    }
     @Override
     public void run() {
-        while (true) {
-            Socket clientSocket;
-            ServerSocket requestListener;
-            try {
-                requestListener = new ServerSocket(8080);
-                clientSocket = requestListener.accept();
-                requestListener.close();
-            } catch (IOException err) {
-                System.err.println("Could not create a socket.");
-                break;
-            } 
-            long start = System.currentTimeMillis();
-            try {
-                while (System.currentTimeMillis() - start < 3000) {
+        try {
+            while (true) {
+                Socket clientSocket = requestQueue.take();
                 InputStream in = clientSocket.getInputStream();
-                    if (in.available() > 0) {
-                        System.out.println(in.available());
-                        HttpRequestHeader clientRequest = HttpRequestHeader.createFromStream(in);
-                        System.out.println("3");
-                        OutputStream out = clientSocket.getOutputStream();
-                        HttpResponse response = HttpResponse.createResponseFromHeader(clientRequest);
-                        System.out.println("4");
-                        response.send(out);
-                        out.close();
-                        System.out.println("5");
-                        start = System.currentTimeMillis();
-                    }
-                    in.close();
-                }
+                OutputStream out = clientSocket.getOutputStream();
+                HttpRequestHeader requestHeader = new HttpRequestHeader().createFromStream(in);
+                HttpResponse response = new HttpResponse().createResponseFromHeader(requestHeader);
+                response.send(out);
                 clientSocket.close();
-                System.out.println("7");
-            } catch (IOException err) {
-                System.err.println("something went wrong.");
-                err.printStackTrace();
             }
+        } catch (InterruptedException err) {
+            err.printStackTrace();
+        } catch (IOException err) {
+            err.printStackTrace();
         }
     }
 }
